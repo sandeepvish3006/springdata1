87473401b50d457fa54e7bd1d3e060ef5cf70b2c
SANDEEP VISHWAKARMA <sandeepvish3006@gmail.com>
2017-09-26 18:25:09 +0530

    Add files via upload
    add files

---
+++ src/test/java/springdata1/controller/UserController.java
@@ -0,0 +1,61 @@
+package springdata1.controller;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import springdata1.dao.UserRepository;
+import springdata1.model.User;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+	@Autowired
+	UserRepository userRepository;
+
+	@GetMapping
+	@RequestMapping("/add")
+	public @ResponseBody String addUser(@RequestParam String name, @RequestParam String email) {
+		User user = new User();
+		user.setName(name);
+		user.setEmail(email);
+		userRepository.save(user);
+
+		return "saved";
+	}
+
+	@GetMapping("/getAll")
+	public @ResponseBody Iterable<User> getAllUser() {
+		return userRepository.findAll();
+	}
+
+	@GetMapping("/findByEmail")
+	public @ResponseBody Iterable<User> getByEmail(@RequestParam String email) {
+		return userRepository.findByEmail(email);
+	}
+
+	@GetMapping("/findByName")
+	public @ResponseBody User findByName(@RequestParam String name) {
+		return userRepository.findByName(name);
+	}
+
+	@GetMapping("/findByNameOrEmail")
+	public @ResponseBody Iterable<User> findByNameOrEmail(@RequestParam String name, @RequestParam String email) {
+		return userRepository.findByNameOrEmail(name, email);
+	}
+	
+	@Modifying
+	@Transactional
+	@GetMapping("/updateByEmail")
+	public @ResponseBody int updateByEmail(@RequestParam String name, @RequestParam String email){
+		return userRepository.updateByEmail(name,email);
+		
+	}
+}
---
+++ src/test/java/springdata1/dao/UserRepository.java
@@ -0,0 +1,25 @@
+package springdata1.dao;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import springdata1.model.User;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long>{
+	
+	Iterable<User> findByEmail(String email);
+	
+	@Query("select u from User u where u.name = ?")
+	User findByName(String name);
+	
+	@Query("select u from User u where u.name = ? or u.email=?")
+	Iterable<User> findByNameOrEmail(@Param("name") String name, @Param("email") String email);
+	
+	@Modifying
+	@Query("update User u set u.name = ? where u.email = ?")
+	int updateByEmail(@Param("name") String name, @Param("email") String email);
+
+}
